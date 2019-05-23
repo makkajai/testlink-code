a450cbf48df5a362775d720f045c5d99780c8884
Roopesh Shenoy <shenoy.roopesh@gmail.com>
2019-05-23 18:22:57 +0530

    Adding config_db.inc.php so it goes with the application and avoids installation

---
+++ config_db.inc.php
@@ -0,0 +1,6 @@
+define('DB_TYPE', 'postgres');
+define('DB_USER', 'testlinkuser');
+define('DB_PASS', 'testLinkUser@makkajai123');
+define('DB_HOST', 'monster-math-competition-db.c3exe9kzzi00.us-west-2.rds.amazonaws.com:5432');
+define('DB_NAME', 'Testlink');
+define('DB_TABLE_PREFIX', '');
